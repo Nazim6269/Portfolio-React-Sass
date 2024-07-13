6e4a25859968cc41e0c0591b9d3f943d0f4eb79a
nazimuddin <nazimuddin102001@gmail.com>
2024-07-13 12:52:25 +0600

    completed contact section

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -1,3 +1,5 @@
+import { Button } from "@mui/material";
+import { motion } from "framer-motion";
 import { useState } from "react";
 import "./contact.scss";
 
@@ -12,15 +14,33 @@ export default function Contact() {
   return (
     <div className="contact" id="contact">
       <h1>Get Contact</h1>
-      <div className="left">{/* <img src="assets/shake.svg" alt="" /> */}</div>
-      <div className="right">
-        <h2>Contact.</h2>
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email" />
-          <textarea placeholder="Message"></textarea>
-          <button type="submit">Send</button>
-          {message && <span>Thanks, I&apos;ll reply ASAP :</span>}
-        </form>
+      <div className="leftWrapper">
+        <div className="left">
+          <motion.img
+            animate={{ rotate: 360 }}
+            transition={{
+              repeat: Infinity,
+              duration: 8,
+              ease: "linear",
+            }}
+            src="reactNoBackground.png"
+            alt=""
+          />
+        </div>
+        <div className="right">
+          <h2>Contact</h2>
+          <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Email" />
+            <textarea placeholder="Message"></textarea>
+            <div className="btn">
+              <Button variant="contained" className="button">
+                {" "}
+                Send
+              </Button>
+            </div>
+            {message && <span>Thanks, I&apos;ll reply ASAP :</span>}
+          </form>
+        </div>
       </div>
     </div>
   );
