245c3a9fe354830216d516519ede7617b27adc67
nazimuddin <nazimuddin102001@gmail.com>
2024-07-16 22:42:04 +0600

    completed modal functionality

--- src/Components/menuModal/MenuModal.jsx
+++ src/Components/menuModal/MenuModal.jsx
@@ -2,21 +2,28 @@ import { Button } from '@mui/material';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { navArray } from '../../data';
-import './_menuModal.scss'; // Ensure you have your SCSS imported
+import './_menuModal.scss';
 
 const MenuModal = ({ onClose }) => {
+  const handleClick = () => {
+    onClose();
+  };
+
   return (
     <div className="menu-modal">
       <ul className="menu-modal__list">
         {navArray.map((item) => (
           <li key={item.id} className="menu-modal__list-item">
-            <Link to={item.path}>{item.name}</Link>
+            <Link to={item.path} onClick={handleClick}>
+              {item.name}
+            </Link>
           </li>
         ))}
+
+        <Button variant="outlined" size="small" onClick={onClose}>
+          Close
+        </Button>
       </ul>
-      <Button variant="outlined" size="small" onClick={onClose}>
-        Close
-      </Button>
     </div>
   );
 };
