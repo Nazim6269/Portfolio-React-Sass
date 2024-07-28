570741f9e318b3db2ea0a636d96a45eea824b4f0
nazimuddin <nazimuddin102001@gmail.com>
2024-07-28 12:17:09 +0600

    implemented counter multiple times when it comes to viewport

--- src/Components/counterAnimation/CounterAnimation.jsx
+++ src/Components/counterAnimation/CounterAnimation.jsx
@@ -1,17 +1,42 @@
 import { animate, motion, useMotionValue, useTransform } from 'framer-motion';
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const CounterAnimation = () => {
+  const [hasCounted, setHasCounted] = useState(false);
   const count = useMotionValue(0);
   const roundedValue = useTransform(count, Math.round);
+  const counterRef = useRef(null);
 
   useEffect(() => {
-    const animation = animate(count, 100, { duration: 2 });
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          if (!hasCounted) {
+            setHasCounted(true);
+          }
+          const animation = animate(count, 100, { duration: 2 });
+          return () => animation.stop();
+        } else {
+          count.set(0);
+          setHasCounted(false);
+        }
+      },
+      { threshold: 0.5 },
+    );
 
-    return animation.stop;
-  }, [count]);
+    const currentElement = counterRef.current;
+    if (currentElement) {
+      observer.observe(currentElement);
+    }
+
+    return () => {
+      if (currentElement) {
+        observer.unobserve(currentElement);
+      }
+    };
+  }, [count, counterRef, hasCounted]);
   return (
-    <div className="animation__container">
+    <div className="animation__container" ref={counterRef}>
       <div className="animation__wrapper">
         <h1>Acknowledgements</h1>
         <div className="animation__items">
